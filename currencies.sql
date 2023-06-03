624b7e581abf9dcbffd4deda994123ceb1499476
jobir307 <50763117+jobir307@users.noreply.github.com>
2023-06-03 06:59:08 +0300

    Update currencies.sql

--- currencies.sql
+++ currencies.sql
@@ -16,8 +16,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-https://www.youtube.com/watch?v=sEwFtQRRvos
-https://www.youtube.com/watch?v=ZEUpx-QRELA
 --
 -- Database: `ibnsino`
 --
